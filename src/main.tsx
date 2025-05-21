bdcf02bf736ecb52bef9ab9d016a3a3a40bc73cf
TaYaKi71751 <shantian.tw@gmail.com>
2025-05-21 14:41:38 +0900

    Use redux persist

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,19 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { Provider } from 'react-redux'
-import { store } from './store.ts'
+import { store, persistor} from './store'
+
+
+import { PersistGate } from "redux-persist/integration/react";
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+       <PersistGate loading={null} persistor={persistor}>
+    <App />
+    </PersistGate>
     </Provider>
   </StrictMode>,
 )
+
